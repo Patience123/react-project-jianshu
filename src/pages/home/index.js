7e557f2c8640976eda5241ededadc800ad463ed0
Patience123 <773581321@qq.com>
2018-11-12 15:36:21 +0800

    feat: 首页整体布局&组件拆分

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import Topic from './components/Topic';
+import List from './components/List';
+import Recommend from './components/Recommend';
+import Writer from './components/Writer';
+import {
+    HomeWrapper,
+    HomeLeft,
+    HomeRight
+} from './styled';
 
 class Home extends Component {
     render() {
         return (
-            <div>Home</div>
+            <HomeWrapper>
+                <HomeLeft>
+                    <img className="banner-img" src="//upload.jianshu.io/admin_banners/web_images/4574/d5c5ea3e984c8c08071b467c2dfe5bd2d0acf7b8.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540" alt="banner-img" />
+                    <Topic></Topic>
+                    <List></List>
+                </HomeLeft>
+                <HomeRight>
+                    <Recommend></Recommend>
+                    <Writer></Writer>
+                </HomeRight>
+            </HomeWrapper>
         )
     }
 }
---
+++ src/pages/home/styled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const HomeWrapper = styled.div`
+    width: 960px;
+    overflow: hidden;
+    margin: 0 auto;
+    margin-top: 30px;
+`
+
+export const HomeLeft = styled.article`
+    float: left;
+    width: 625px;
+    margin-right: 15px;
+    .banner-img {
+        width: 100%;
+        height: 270px;
+    }
+`
+
+export const HomeRight = styled.aside`
+    float: right;
+    width: 320px;
+`
