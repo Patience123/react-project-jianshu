840ad693fcb228843e1c86bd2c8c91b1ac861d0a
Patience123 <773581321@qq.com>
2018-11-12 14:51:48 +0800

    feat: 添加路由配置

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
-import Heador from './common/header';
 import store from './store';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Heador from './common/header';
+import Home from './pages/home';
+import Detail from './pages/detail';
 
 class App extends Component {
 	render() {
 		return (
 			<Provider store={store}>
-				<Heador></Heador>
+				<React.Fragment>
+					<Heador />
+					<BrowserRouter>
+						<React.Fragment>
+							<Route path="/" exact component={Home} />
+							<Route path="/detail" exact component={Detail} />
+						</React.Fragment>
+					</BrowserRouter>
+				</React.Fragment>
 			</Provider>
 		);
 	}
